ca1ded9a69c1d837d197b717aa1a7c80dfce0e2b
mregnauld <mregnauld@gmail.com>
2020-01-13 13:01:38 +0100

    Mise a jour Android 10

--- app/src/main/java/com/formationandroid/asynctask/MainActivity.java
+++ app/src/main/java/com/formationandroid/asynctask/MainActivity.java
@@ -1,8 +1,6 @@
 package com.formationandroid.asynctask;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Date;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity
 {
